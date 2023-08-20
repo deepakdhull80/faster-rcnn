c8c7126be14b99f1da5fef1c9b5623176c88f5aa
deepakdhull80 <deepakdhull80@gmail.com>
2023-08-20 23:07:02 +0530

    preprocess annotation file

--- data/prepare_data.py
+++ data/prepare_data.py
@@ -9,7 +9,10 @@ import zipfile
 import os
 import requests
 import sys
+import json
+import pandas as pd
 from clint.textui import progress
+from tqdm import tqdm
 
 ANNOTATION_PATH = 'data/annotations'
 IMAGE_PATH = "data/images"
@@ -32,9 +35,22 @@ def unzip(file, folder):
         print(f"Extracted contents from '{file}' to '{folder}' directory.")
 
 def preprocess_coco_annotations(filename):
-    annotation = json.load(open(filename))
-    
+    ds = json.load(open(filename))
+    images = pd.DataFrame(ds['images'])[['file_name', 'id']]
+    annotations = pd.DataFrame(ds['annotations'])[['image_id', 'bbox', 'category_id']]
+    merge_df = pd.merge(images, annotations, how='inner', left_on='id', right_on='image_id')
+    def helper(g_df):
+        f = list(zip(g_df['bbox'].tolist(), g_df['category_id'].tolist()))
+        return f
 
+    f_df = merge_df.groupby(['file_name', 'image_id']).apply(helper).reset_index()
+    f_df = f_df.rename(columns={0:"box_category"})
+
+    for _, row in tqdm(merge_df.iterrows(), total=merge_df.shape[0]):
+        filename = row['file_name'].replace(".jpg",'.json')
+        json.dump(row.to_dict(),open(f"{ANNOTATION_PATH}/{filename}",'w'))
+    
+    
 def prepare_coco_dataset():
     images_url = "http://images.cocodataset.org/zips/train2017.zip"
     annotations_url = "http://images.cocodataset.org/annotations/annotations_trainval2017.zip"
@@ -42,8 +58,11 @@ def prepare_coco_dataset():
     
     download(annotations_url, prefix_path.format(file_name='annotation'))
     unzip(prefix_path.format(file_name='annotation'), ANNOTATION_PATH)
+    preprocess_coco_annotations(f"{ANNOTATION_PATH}/annotations/instances_train2017.json")
     
-    # download(images_url, prefix_path.format(file_name='images'))
+    download(images_url, prefix_path.format(file_name='images'))
+    unzip(prefix_path.format(file_name='images'), IMAGE_PATH)
+
 
 if __name__ == "__main__":
     prepare_coco_dataset()
